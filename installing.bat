5b600a60307c15b5b30bb9fc9facc477e737035e
Juan Romo <52178179+juanmaro97@users.noreply.github.com>
2019-09-17 18:49:17 -0500

    commit 1
    primer commit de wanda y romo

---
+++ installing.bat
@@ -0,0 +1,3 @@
+pip install pykinect2
+:: read the thread https://github.com/Kinect/PyKinect2/issues/37 if issue
+pip install pygame
---
+++ prueba.py
@@ -0,0 +1,29 @@
+
+
+class Snake:
+    #
+    #    hola soy una funcion de serpientes
+    #   sdjfksdf
+    #
+    def __init__(self, name: str = "culebris"):
+
+        self.name = name
+        print("hi i am a snake")
+        
+    def print_name(self):
+        print(self.name)
+
+
+
+if __name__ == "__main__":
+
+    class Obj:
+        def __init__(self):
+            pass
+
+    o = Obj()
+    my_snake = Snake(o)
+    my_snake.print_name()
+
+    
+    
